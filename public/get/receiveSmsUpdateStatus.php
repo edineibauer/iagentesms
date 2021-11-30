ea3a37b8b4f6d61e581451960ed71be496b24294
edineijacobbauer <edineibauer@gmail.com>
2021-11-30 17:37:41 -0300

    remove create arquivo

--- public/get/receiveSmsUpdateStatus.php
+++ public/get/receiveSmsUpdateStatus.php
@@ -8,8 +8,5 @@ $datatime = explode(" ", $dados["data"]);
 $datatime2 = explode("/", $datatime[0]);
 $datatime = $datatime2[2] . "-" . $datatime2[1] . "-" . $datatime2[0] . " " . $datatime[1];
 
-\Config\Config::createFile(PATH_HOME . "sms-" . time() . ".json", json_encode($dados));
-\Config\Config::createFile(PATH_HOME . "sms2-" . time() . ".json", $_SERVER['REQUEST_URI']);
-
 $up = new \Conn\Update();
 $up->exeUpdate("smsiagente", ["status" => $dados["status"], "data" => $datatime], "WHERE id = :c", "c={$dados["codigosms"]}");
