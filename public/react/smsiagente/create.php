81154167edad2015eb0f92467d5cab7d036c825f
Edinei J. Bauer <galera.org@gmail.com>
2025-02-17 18:13:34 -0300

    registra erro ao enviar a requisição para a SMS iagente caso tenha erro

--- public/react/smsiagente/create.php
+++ public/react/smsiagente/create.php
@@ -6,6 +6,13 @@ try {
     $sms->setMensagem($dados['mensagem']);
     $sms->setId($dados['id']);
     $sms->envia();
+
+    if(!empty($sms->getError())) {
+        $up = new \Conn\Update();
+        $up->exeUpdate("smsiagente", ["status" => "Erro: " . $sms->getError()], "WHERE id = :id", ["id" => $dados['id']]);
+        $data['error'] = $sms->getError();
+    }
+
 } catch (Exception $e) {
     //Não faz nada com possíveis erros
 }
